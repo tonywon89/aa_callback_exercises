d863de96b3efc98d05e891ec2ad39d3679a4814b
Tony Won <tonywon89@gmail.com>
2016-04-08 15:17:01 -0700

    Implement absurdBubbleSort

--- absurd_bubblesort.js
+++ absurd_bubblesort.js
@@ -15,7 +15,40 @@ function askIfGreaterThan (el1, el2, callback) {
   });
 }
 
-askIfGreaterThan(3, 6, function (answer) {
-  console.log("Here is the " + answer);
+// askIfGreaterThan(3, 6, function (answer) {
+//   console.log("Here is the " + answer);
+//   reader.close();
+// });
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+  if (i < arr.length - 1) {
+    askIfGreaterThan(arr[i], arr[i + 1], function (isGreaterThan) {
+      if (isGreaterThan) {
+        var temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        madeAnySwaps = true;
+      }
+      innerBubbleSortLoop(arr, i + 1, madeAnySwaps, outerBubbleSortLoop);
+    });
+  } else {
+    outerBubbleSortLoop(madeAnySwaps);
+  }
+}
+
+function absurdBubbleSort (arr, sortCompletionCallback) {
+  function outerBubbleSortLoop (madeAnySwaps) {
+    if (madeAnySwaps) {
+      innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+    } else {
+      sortCompletionCallback(arr);
+    }
+  }
+
+  outerBubbleSortLoop(true);
+}
+
+absurdBubbleSort([4, 2, 1, 3], function(array) {
+  console.log(array);
   reader.close();
 });
